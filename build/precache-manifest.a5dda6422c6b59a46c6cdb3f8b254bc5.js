793662b6ecc9bfb2261b6379fd21bf7c12bdd707
Felipe Schultz <felipe.schultz@softdesign.com.br>
2019-10-09 15:16:03 -0300

    Teste deploy

---
+++ build/precache-manifest.a5dda6422c6b59a46c6cdb3f8b254bc5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c2d3f2cb067b0eff941725c8f3bfd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4684e6f986e2276460de",
+    "url": "/static/css/main.979346aa.chunk.css"
+  },
+  {
+    "revision": "e38e1a49b823c4aa697e",
+    "url": "/static/js/2.1187fb28.chunk.js"
+  },
+  {
+    "revision": "4684e6f986e2276460de",
+    "url": "/static/js/main.61df44dd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a78d37de43d4f92a07591ad6e9463c4b",
+    "url": "/static/media/Group.a78d37de.svg"
+  },
+  {
+    "revision": "7461e11b938c1de1c92b8b3444e28811",
+    "url": "/static/media/login-backgorund.7461e11b.jpg"
+  },
+  {
+    "revision": "7faafd8530ca47f8a0ad857ad6c1a56a",
+    "url": "/static/media/logo-green.7faafd85.svg"
+  },
+  {
+    "revision": "559450a05d639ca723f9fca32b890c5a",
+    "url": "/static/media/logo.559450a0.svg"
+  }
+]);
